f3c79bbe20d8a163e2b4a6eb3f79270e0a4f62a2
Raja Pasupuleti <raja.pasupuleti@gatech.edu>
2017-12-18 11:40:51 -0500

    with babel updates

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
-var chalk = require("chalk");
+//var chalk = require("chalk"); //  standard JS
+/*jshint esversion: 6 */
+import chalk from "chalk";// ES 6
 
 console.log(chalk.green("starting application"));
